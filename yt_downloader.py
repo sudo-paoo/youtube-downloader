77bb59cc9e85cb23182bb1d65590db7eaaac4a31
Pao <104471005+sudo-paoo@users.noreply.github.com>
2022-06-14 12:10:12 +0800

    Add files via upload

---
+++ yt_downloader.py
@@ -0,0 +1,70 @@
+import youtube_dl
+import re
+import os
+links = []
+
+def youtube_url_validation(url):
+    youtube_regex = (
+        r'(https?://)?(www\.)?'
+        '(youtube|youtu|youtube-nocookie)\.(com|be)/'
+        '(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})')
+
+    youtube_regex_match = re.match(youtube_regex, url)
+    if youtube_regex_match:
+        return youtube_regex_match
+
+    return youtube_regex_match
+
+def download_mp4(url):
+    for yt_url in url:
+        video_url = yt_url
+        video_info = youtube_dl.YoutubeDL().extract_info(
+            url=video_url, download = False
+            )
+        file = f"{video_info['title']}"
+        options={
+            'format':'bestvideo+bestaudio',
+            'outtmpl':f"./downloads/mp4/{file}",
+            'ffmpeg-location': './'
+        }
+        with youtube_dl.YoutubeDL(options) as ydl:
+            ydl.download([video_info['webpage_url']])
+        print(f"Download Success: {file}")
+        os.system("pause")
+
+def download_mp3(url):
+    for yt_url in url:
+        video_url = yt_url
+        video_info = youtube_dl.YoutubeDL().extract_info(url = video_url,download=False)
+        filename = f"{video_info['title']}.mp3"
+        options={
+            'format':'bestaudio/best',
+            'keepvideo':False,
+            'outtmpl':f"./downloads/mp3/{filename}"
+        }
+        with youtube_dl.YoutubeDL(options) as ydl:
+            ydl.download([video_info['webpage_url']])
+        print(f"Download Success: {filename}")
+        os.system("pause")
+
+while True:
+    link = input('Youtube URL: ')
+    validator = youtube_url_validation(link)
+    if str.lower(link) == 'mp4':
+        for i in links:
+            print("The following urls will be downloaded")
+            print(f"URL: {i}")
+        download_mp4(links)
+        break
+    elif str.lower(link) == 'mp3':
+        for i in links:
+            print("The following urls will be downloaded")
+            print(f"URL: {i}")
+        download_mp3(links)
+        break
+    elif link != 'mp4' or 'mp3':
+        if validator:
+            links.append(link)
+            print(f"Valid URL.")
+        else:
+            print(f"Invalid URL.")
